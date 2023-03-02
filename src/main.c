041da23427375e0475595bb5b8c4b4c690b0b6c9
James.Y.Yang (tech.cncd02.Newegg) 42035 <james.yang516@outlook.com>
2023-03-02 21:05:42 +0800

    improve

--- src/main.c
+++ src/main.c
@@ -26,29 +26,73 @@ void wrapStrFromPTR(char* str, size_t len, const char* head, const char* tail) {
   str[len - 1] = '\0';
 }
 
-void tryParseUrl(char* req) {
-  // extract uri;
-  const char* uriHead = strchr(req, ' ') + 1;
-  const char* uriTail = strchr(uriHead, ' ');
-  size_t uriLen = uriTail - uriHead + 1;
-  char strUri[uriLen];
-  wrapStrFromPTR(strUri, uriLen, uriHead, uriTail);
-
-  printf("raw url: %s\n", strUri);
-
-  // parse uri;
-  UriUriA uri;
-  UriQueryListA* queryList;
-  const char* errorPos;
-  if (uriParseSingleUriA(&uri, strUri, &errorPos) == URI_SUCCESS) {
-    printf("parse url success: %s\n", strUri);
-  }
-}
-
 void error_die(const char *sc)
 {
   perror(sc); 
   exit(1);
+}
+
+void unimplemented(int client)
+{
+ char buf[1024];
+
+ sprintf(buf, "HTTP/1.0 501 Method Not Implemented\r\n");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "Server: jecho");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "Content-Type: text/html\r\n");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "\r\n");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "<HTML><HEAD><TITLE>Method Not Implemented\r\n");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "</TITLE></HEAD>\r\n");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "<BODY><P>HTTP request method not supported.\r\n");
+ send(client, buf, strlen(buf), 0);
+ sprintf(buf, "</BODY></HTML>\r\n");
+ send(client, buf, strlen(buf), 0);
+}
+
+void accept_request(int acceptedSocket){
+
+  char reqBuf[HTTP_REQ_BUF];
+  bzero(reqBuf, HTTP_REQ_BUF); 
+
+  const size_t receivedBytes = read(acceptedSocket, reqBuf, HTTP_REQ_BUF);
+
+  const char* uriHead = strchr(reqBuf, ' ') + 1;
+  const char* uriTail = strchr(uriHead, ' ');
+  size_t methodLen = uriHead - reqBuf;
+  char strMethod[methodLen];
+  wrapStrFromPTR(strMethod, methodLen, reqBuf, uriHead);
+  printf("http method: %s\n", strMethod);
+
+  if (strcasecmp(strMethod, "GET"))
+  {
+    unimplemented(acceptedSocket);
+    close(acceptedSocket);
+    return;
+  }
+
+  size_t uriLen = uriTail - uriHead + 1;
+  char strUri[uriLen];
+  wrapStrFromPTR(strUri, uriLen, uriHead, uriTail);
+  printf("raw url: %s\n", strUri);
+
+  char resBuf[HTTP_RES_BUF];
+
+  time_t currTime = time(NULL);
+  struct tm* tm = localtime(&currTime);
+
+  sprintf(resBuf, "HTTP/1.1 200 OK\r\n"
+                  "Content-type: text/plain\r\n\r\n"
+                  "Hello, you are connected, this is welcome msg from Jecho\r\n"
+                  "current time: %s", asctime(tm));
+
+  write(acceptedSocket, resBuf, strlen(resBuf));
+
+  close(acceptedSocket);
 }
 
 void startServer() {
@@ -96,29 +140,7 @@ int main(int argc, const char* argv[]) {
       error_die("In accept");
     }
 
-    char reqBuf[HTTP_REQ_BUF];
-    bzero(reqBuf, HTTP_REQ_BUF); 
-
-    const size_t receivedBytes = read(acceptedSocket, reqBuf, HTTP_REQ_BUF);
-
-    if (receivedBytes > 0) {
-      printf("you send msg: \n%s", reqBuf);
-      tryParseUrl(reqBuf);
-    }
-
-    char resBuf[HTTP_RES_BUF];
-
-    time_t currTime = time(NULL);
-    struct tm* tm = localtime(&currTime);
-
-    sprintf(resBuf, "HTTP/1.1 200 OK\r\n"
-                    "Content-type: text/plain\r\n\r\n"
-                    "Hello, you are connected, this is welcome msg from Jecho\r\n"
-                    "current time: %s", asctime(tm));
-
-    write(acceptedSocket, resBuf, strlen(resBuf));
-
-    close(acceptedSocket);
+    accept_request(acceptedSocket);
 
   }
 
